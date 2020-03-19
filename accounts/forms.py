da450845b2f61e80dfc6fb1286360c1fef1e2773
NumanIbnMazid <numanibnmazid@gmail.com>
2020-03-20 02:20:02 +0600

    Bug Fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -152,6 +152,13 @@ class BaseUserForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
 
+    def save(self, commit=True):
+        user = super(BaseUserForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
 
 class AdminUserProfileForm(forms.ModelForm):
     class Meta:
